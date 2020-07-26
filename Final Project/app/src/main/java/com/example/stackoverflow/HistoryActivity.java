036738b425208ae8110b473d8b952ba7c7785ef1
wing2005 <itamargl@gmail.com>
2020-07-26 21:04:41 +0300

    added PostViewActivity implemented SwipeToDeleteCallback with undo fixed dispatcher reset on MainActivity

--- Final Project/app/src/main/java/com/example/stackoverflow/HistoryActivity.java
+++ Final Project/app/src/main/java/com/example/stackoverflow/HistoryActivity.java
@@ -1,18 +1,24 @@
 package com.example.stackoverflow;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-
 import com.example.stackoverflow.adapter.HistoryAdapter;
+import com.example.stackoverflow.handlers.SwipeToDeleteCallback;
 import com.example.stackoverflow.model.Search;
 import com.example.stackoverflow.services.DBService;
 import com.example.stackoverflow.services.DispatcherService;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -21,7 +27,7 @@ public class HistoryActivity extends AppCompatActivity {
     private DBService dbService;
     private List<Search> searches;
     private RecyclerView historyRecycler;
-
+    private HistoryAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +36,10 @@ public class HistoryActivity extends AppCompatActivity {
         searches = dbService.getSearches();
         historyRecycler = findViewById(R.id.historyRecyclerView);
 
-        HistoryAdapter adapter = new HistoryAdapter(searches);
-        historyRecycler.setAdapter(adapter);
+        mAdapter = new HistoryAdapter(searches);
+        historyRecycler.setAdapter(mAdapter);
         historyRecycler.setLayoutManager(new LinearLayoutManager(HistoryActivity.this));
-
+        enableSwipeToDeleteAndUndo();
     }
 
 
@@ -47,5 +53,47 @@ public class HistoryActivity extends AppCompatActivity {
 
     public void gohome(View view) {
         startActivity(new Intent(this,MainActivity.class));
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+
+                final int position = viewHolder.getAdapterPosition();
+                final Search item = mAdapter.getSearches().get(position);
+
+                mAdapter.removeItem(position);
+
+                LinearLayout linearLayout = findViewById(R.id.historyLinear);
+                Snackbar snackbar = Snackbar
+                        .make(linearLayout, "Item was removed from the list.", Snackbar.LENGTH_LONG)
+                        .addCallback(new BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                            public void onDismissed(Snackbar transientBottomBar, int event) {
+                                if (event != DISMISS_EVENT_ACTION) {
+                                    // Dismiss wasn't because of tapping "UNDO"
+                                    dbService.deleteSearch(item.getSearchID());
+                                }
+                            }
+                        });
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        mAdapter.restoreItem(item, position);
+                        historyRecycler.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+
+
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(historyRecycler);
     }
 }
