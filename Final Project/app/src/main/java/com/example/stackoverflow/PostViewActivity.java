b779596fdf19c8ca11a6bec2b254b26d6263b8c5
amitm <amitmarkus1@gmail.com>
2020-07-30 19:16:44 +0300

    videoview+mediaplayer+webview

--- Final Project/app/src/main/java/com/example/stackoverflow/PostViewActivity.java
+++ Final Project/app/src/main/java/com/example/stackoverflow/PostViewActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.stackoverflow.services.DispatcherService;
 
 public class PostViewActivity extends BaseActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,9 @@ public class PostViewActivity extends BaseActivity {
         SharedPreferences prefs = getSharedPreferences(MainActivity.PREFS, MODE_PRIVATE);
         String postUrl = prefs.getString(MainActivity.KEY_POST_URL, "https://stackoverflow.com/");
 
+        webView=findViewById(R.id.webviewid);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(postUrl);
     }
 
     public void gohome(View view) {
